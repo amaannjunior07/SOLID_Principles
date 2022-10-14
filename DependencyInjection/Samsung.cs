5cb1e602b6cb2c3a5c15e79120c9718433e47dfb
WinWire <WinWire@DESKTOP-6M8FF3N>
2022-10-14 15:41:39 +0530

    Add project files.

---
+++ DependencyInjection/Samsung.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles.DependencyInjection
+{
+    class SamsungNote : apple
+    {
+        public double GetPart1Cost()
+        {
+            return 30000;
+        }
+
+        public string GetPhonePart1()
+        {
+            return "Display";
+        }
+    }
+}
+
---
+++ DependencyInjection/iPhone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles.DependencyInjection
+{
+    class Redmi7 : apple
+    {
+        public double GetPart1Cost()
+        {
+            return 20000;
+        }
+
+        public string GetPhonePart1()
+        {
+            return "Display";
+        }
+    }
+}
